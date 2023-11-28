873f5e810e8801a3d8c5b4d0dc0b8b76cfc2ea30
zondaf12 <ruaridhbell73@googlemail.com>
2023-11-28 00:27:34 +0000

    Updated to use differnet .env in prod

--- config/viper.go
+++ config/viper.go
@@ -1,20 +1,37 @@
 package config
 
-import "github.com/spf13/viper"
+import (
+	"os"
+
+	"github.com/spf13/viper"
+)
 
 type EnvVars struct {
-	MONGODB_URI  string `mapstructure:"MONGODB_URI"`
-	DATABASE string `mapstructure:"DATABASE"`
-	PORT         string `mapstructure:"PORT"`
+	MONGODB_URI    string `mapstructure:"MONGODB_URI"`
+	DATABASE       string `mapstructure:"DATABASE"`
+	PORT           string `mapstructure:"PORT"`
 	AUTH0_DOMAIN   string `mapstructure:"AUTH0_DOMAIN"`
 	AUTH0_AUDIENCE string `mapstructure:"AUTH0_AUDIENCE"`
-	DVLA_API string `mapstructure:"DVLA_API"`
-	DVSA_API string `mapstructure:"DVSA_API"`
+	DVLA_API       string `mapstructure:"DVLA_API"`
+	DVSA_API       string `mapstructure:"DVSA_API"`
 }
 
 func LoadConfig() (config EnvVars, err error) {
+	env := os.Getenv("GO_ENV")
+	if env == "production" {
+		return EnvVars{
+			MONGODB_URI:    os.Getenv("MONGODB_URI"),
+			DATABASE:       os.Getenv("DATABASE"),
+			PORT:           os.Getenv("PORT"),
+			AUTH0_DOMAIN:   os.Getenv("AUTH0_DOMAIN"),
+			AUTH0_AUDIENCE: os.Getenv("AUTH0_AUDIENCE"),
+			DVLA_API:       os.Getenv("DVLA_API"),
+			DVSA_API:       os.Getenv("DVSA_API"),
+		}, nil
+	}
+
 	viper.AddConfigPath(".")
- 	viper.SetConfigName("app")
+	viper.SetConfigName("app")
 	viper.SetConfigType("env")
 
 	viper.AutomaticEnv()
